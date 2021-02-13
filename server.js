4d9139f1c50d775dd0391c7064a1982fb25b6d1e
ShannonDraper <shan.draper@gmail.com>
2021-02-13 12:57:21 -0500

    express build

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const app = express()
 const port = process.env.PORT || 5000
 const request = require('request')
 const fetch = require('node-fetch')
+const path = require('path');
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 app.get('/weather', async (request, response) => {
