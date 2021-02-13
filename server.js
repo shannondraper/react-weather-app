58b20c16537cc2f4b223354cdf0e172ff7de678f
ShannonDraper <shan.draper@gmail.com>
2021-02-13 14:24:48 -0500

    start script

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ const port = process.env.PORT || 5000
 const request = require('request')
 const fetch = require('node-fetch')
 const path = require('path');
+const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
 });
-
 
 app.get('/weather', async (request, response) => {
     const weatherURL = `https://api.darksky.net/forecast/${process.env.REACT_APP_DARKSKY_KEY}/43.585891,-79.5835271?units=si`;
@@ -23,4 +22,4 @@ app.get('/weather', async (request, response) => {
     response.json(json);
 })
 
-app.listen(port, () => console.log(`listening on port: ${port}`))
+app.listen(port, () => console.log(`server is listening on port: ${port}`))
